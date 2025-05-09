8972e6e099e7deb4b481e10494f82d478b5095f8
Greyfeather <HighTideWrites@gmail.com>
2025-05-08 23:03:51 -0600

    add jsdoc to materialicon.js

--- .config/ags/modules/.commonwidgets/materialicon.js
+++ .config/ags/modules/.commonwidgets/materialicon.js
@@ -1,5 +1,12 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 
+/**
+ * 
+ * @param {string} icon 
+ * @param {number} size 
+ * @param {{}} props 
+ * @returns 
+ */
 export const MaterialIcon = (icon, size, props = {}) => Widget.Label({
     className: `icon-material txt-${size}`,
     label: icon,
