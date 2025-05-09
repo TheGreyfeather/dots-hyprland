d6bd88a816fc8655c144fa074549b4f970985e09
Greyfeather <HighTideWrites@gmail.com>
2025-05-08 23:05:43 -0600

    add comments

--- .config/ags/modules/.commonwidgets/materialicon.js
+++ .config/ags/modules/.commonwidgets/materialicon.js
@@ -2,9 +2,9 @@ import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 
 /**
  * 
- * @param {string} icon 
- * @param {number} size 
- * @param {{}} props 
+ * @param {string} icon the name of the icon from the Material Icon set
+ * @param {number} size the desired size of the icon
+ * @param {{}} props Primarily from Gtk.Label
  * @returns 
  */
 export const MaterialIcon = (icon, size, props = {}) => Widget.Label({
